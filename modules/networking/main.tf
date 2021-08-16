b144038d72e355ca8bc2e59825338a9e260a6e7e
sriharshakt <sriharsha0333@gmail.com>
2021-08-16 01:48:57 +0000

    updated code

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -100,19 +100,53 @@ resource "aws_security_group" "default" {
   description = "Default security group to allow inbound/outbound from the VPC"
   vpc_id      = "${aws_vpc.vpc.id}"
   depends_on  = [aws_vpc.vpc]
+  
+  ingress {
+    from_port = "22"
+    to_port   = "22"
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ ingress {
+    from_port = "443"
+    to_port   = "443"
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   ingress {
-    from_port = "0"
-    to_port   = "0"
-    protocol  = "-1"
-    self      = true
+    from_port = "80"
+    to_port   = "80"
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = "-1"
+    to_port   = "-1"
+    protocol  = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ egress {
+    from_port = "22"
+    to_port   = "22"
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ egress {
+    from_port = "443"
+    to_port   = "443"
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = "0"
-    to_port   = "0"
-    protocol  = "-1"
-    self      = "true"
+    from_port = "80"
+    to_port   = "80"
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
